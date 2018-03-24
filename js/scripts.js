5f4189eec283ae2e119f5b874b960f6714d5d2fd
kelsey guerra <kelseylguerra@gmail.com>
2018-03-23 17:20:18 -0700

    add more stylistic propteries and change js variable scores

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("form#tracker").submit(function(event) {
     event.preventDefault();
+    var nameInput = $("input#name").val();
     var frontback = parseInt($("input:radio[name=frontback]:checked").val());
     var trait = parseInt($("input:radio[name=trait]:checked").val());
     var brain = parseInt($("input:radio[name=brain]:checked").val());
@@ -9,14 +10,15 @@ $(document).ready(function() {
     var score = frontback + brain + truth + interests;
 
     $("#output").show();
-    if (score <=7) {
+    if (score <=10) {
       $("#css").show();
-    } else if (score <=10) {
+    } else if (score <=20) {
       $("#php").show();
     } else {
       $("#dunzo").show();
     }
 
     $("form#tracker").hide();
+    $(".name").text(nameInput);
   });
 });
