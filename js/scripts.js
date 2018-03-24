9d5fd64e0faaa25ff547f148769b0c7c4e484144
kelseyguerra <37308564+kelseyguerra@users.noreply.github.com>
2018-03-24 11:01:16 -0700

    "fix variable functions"

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   $("form#tracker").submit(function(event) {
     event.preventDefault();
     var nameInput = $("input#name").val();
-    var frontback = parseInt($("input:radio[name=frontback]:checked").val());
+    var food = parseInt($("input:radio[name=food]:checked").val());
     var trait = parseInt($("input:radio[name=trait]:checked").val());
     var brain = parseInt($("input:radio[name=brain]:checked").val());
     var truth = parseInt($("input:radio[name=truth]:checked").val());
     var interests = parseInt($("input:radio[name=interests]:checked").val());
-    var score = frontback + brain + truth + interests;
+    var score = food + trait + brain + truth + interests;
 
     $("#output").show();
-    if (score <=10) {
+    if (score <=12) {
       $("#css").show();
-    } else if (score <=20) {
+    } else if (score <=30) {
       $("#php").show();
     } else {
       $("#dunzo").show();
